e022d78911fc5ad81558a686244e322b279cf32b
timocapa <timo.capa@gmail.com>
2019-05-12 23:13:21 +0200

    Revert "staging: qcacld-3.0: Fix unused function warning"
    This reverts commit c21a21e561b99e30db080ca873a7fbd3a4d3eb77.

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -12480,7 +12480,6 @@ dev_alloc_err:
 	return -ENODEV;
 }
 
-#ifdef MODULE
 static void wlan_hdd_state_ctrl_param_destroy(void)
 {
 	cdev_del(&wlan_hdd_state_cdev);
@@ -12490,7 +12489,6 @@ static void wlan_hdd_state_ctrl_param_destroy(void)
 
 	pr_info("Device node unregistered");
 }
-#endif
 
 /**
  * __hdd_module_init - Module init helper
